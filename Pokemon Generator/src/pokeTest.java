5490a4576fa741acf85dcc85095a794959d1a570
rfjackson <58374586+rfjackson@users.noreply.github.com>
2020-01-28 10:02:15 +1100

    in test program, added the ability to take user input. There is minimal data validation and only serves to allow custom entry of data for the PokeData object

--- Pokemon Generator/src/pokeTest.java
+++ Pokemon Generator/src/pokeTest.java
@@ -4,10 +4,47 @@ import javax.swing.JOptionPane;
 public class pokeTest {
 
 	public static void main(String[] args) {
+		String species = JOptionPane.showInputDialog("Enter a species: ");
+		String item = JOptionPane.showInputDialog("Enter the item held by the pokemon: ");
+		String IVHPString = JOptionPane.showInputDialog("Enter HP IV: ");
+		String IVatkString = JOptionPane.showInputDialog("Enter attack IV: ");
+		String IVdefString  = JOptionPane.showInputDialog("Enter defense IV: ");
+		String IVspAtkString = JOptionPane.showInputDialog("Enter special attack IV: ");
+		String IVspDefString = JOptionPane.showInputDialog("Enter special defense IV: ");
+		String IVspdString = JOptionPane.showInputDialog("Enter speed IV: ");
+		String EVHPString = JOptionPane.showInputDialog("Enter HP EV: ");
+		String EVatkString = JOptionPane.showInputDialog("Enter attack EV: ");
+		String EVdefString = JOptionPane.showInputDialog("Enter defense EV: ");
+		String EVspAtkString = JOptionPane.showInputDialog("Enter special attack EV: ");
+		String EVspDefString = JOptionPane.showInputDialog("Enter special defense EV: ");
+		String EVspdString = JOptionPane.showInputDialog("Enter speed EV: ");
+		String ability = JOptionPane.showInputDialog("Enter the pokemon's ability: ");
+		String shinyString = JOptionPane.showInputDialog("Is the pokemon shiny?\n[1] Yes\n[2] No");
+		int shinyInt = Integer.parseInt(shinyString);
+		boolean shiny = false;
+		if(shinyInt == 1)
+			shiny = true;
+		else if(shinyInt == 2)
+			shiny = false;
+		String nature = JOptionPane.showInputDialog("Enter the pokemon's nature: ");
+		String move1 = JOptionPane.showInputDialog("Enter move 1: ");
+		String move2 = JOptionPane.showInputDialog("Enter move 2: ");
+		String move3 = JOptionPane.showInputDialog("Enter move 3: ");
+		String move4 = JOptionPane.showInputDialog("Enter move 4: ");
+		int IVHP = Integer.parseInt(IVHPString);
+		int IVatk = Integer.parseInt(IVatkString);
+		int IVdef = Integer.parseInt(IVdefString);
+		int IVspAtk = Integer.parseInt(IVspAtkString);
+		int IVspDef = Integer.parseInt(IVspDefString);
+		int IVspd = Integer.parseInt(IVspdString);
+		int EVHP = Integer.parseInt(EVHPString);
+		int EVatk = Integer.parseInt(EVatkString);
+		int EVdef = Integer.parseInt(EVdefString);
+		int EVspAtk = Integer.parseInt(EVspAtkString);
+		int EVspDef = Integer.parseInt(EVspDefString);
+		int EVspd = Integer.parseInt(EVspdString);
 		try {
-			PokeData testPokemon = new PokeData("Bulbasaur", "A thing", new IVSet(31, 31, 31, 31, 31, 31),
-					new EVSet(252, 252, 6, 0, 0, 0), "things and stuff", true, "beautiful",
-					new MoveSet("Fly", "Splash", "Hydro Pump", "Ice Beam"));
+			PokeData testPokemon = new PokeData(species, item, new IVSet(IVHP, IVatk, IVdef, IVspAtk, IVspDef, IVspd), new EVSet(EVHP, EVatk, EVdef, EVspAtk, EVspDef, EVspd), ability, shiny, nature, new MoveSet(move1, move2, move3, move4));
 			IVSet IVs = testPokemon.getIVs();
 			EVSet EVs = testPokemon.getEVs();
 			MoveSet moves = testPokemon.getMoves();
@@ -36,7 +73,7 @@ public class pokeTest {
 			pokeString += "* Move 2 - " + moves.getMove2() + "\n";
 			pokeString += "* Move 3 - " + moves.getMove3() + "\n";
 			pokeString += "* Move 4 - " + moves.getMove4() + "\n";
-			System.out.println(pokeString);
+			JOptionPane.showMessageDialog(null, pokeString);
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e.getMessage());
 		}
