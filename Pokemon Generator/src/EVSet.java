af089714cd99144b0d999de179b611c20d7ff246
rfjackson <58374586+rfjackson@users.noreply.github.com>
2020-01-28 17:12:06 +1100

    updated EV limits

--- Pokemon Generator/src/EVSet.java
+++ Pokemon Generator/src/EVSet.java
@@ -5,12 +5,12 @@ public class EVSet extends ValueSet {
 
 	/*
 	 * This constructor first calls the parent constructor and throws an EVException
-	 * if any of the EVs are more than 252 or the total EVs total more than 510.
+	 * if any of the EVs are more than 255 or the total EVs total more than 510.
 	 */
 	EVSet(int HP, int atk, int def, int spAtk, int spDef, int spd) throws EVException {
 		super(HP, atk, def, spAtk, spDef, spd);
 		int totalEVs = HP + atk + def + spAtk + spDef + spd;
-		if (HP > 252 || atk > 252 || def > 252 || spAtk > 252 || spDef > 252 || spd > 252 || HP < 0 || atk < 0
+		if (HP > 255 || atk > 255 || def > 255 || spAtk > 255 || spDef > 255 || spd > 255 || HP < 0 || atk < 0
 				|| def < 0 || spAtk < 0 || spDef < 0 || spd < 0 || totalEVs > 510)
 			throw new EVException();
 	}
